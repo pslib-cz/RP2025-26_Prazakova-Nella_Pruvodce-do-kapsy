576035313febfb286b6ef76b7376313fb9715cf6
Nella Pražáková <nella.prazakova.023@pslib.cz>
2026-05-29 21:25:47 +0200

    FIx base

--- pruvodce.client/vite.config.ts
+++ pruvodce.client/vite.config.ts
@@ -5,6 +5,7 @@ const backend = process.env.VITE_API_URL ?? 'http://localhost:5000';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/app/',
   build: {
     outDir: '../pruvodce.server/wwwroot/app',
     emptyOutDir: true,
