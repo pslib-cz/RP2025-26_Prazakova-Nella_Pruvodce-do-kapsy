327e4917637a669020b37668d1092e7f6e90b318
Nella Pražáková <nella.prazakova.023@pslib.cz>
2026-05-29 21:35:41 +0200

    Fix router

--- pruvodce.client/src/App.tsx
+++ pruvodce.client/src/App.tsx
@@ -1,17 +1,15 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import IntroPage from './Pages/IntroPage';
 import MapPage from './Pages/MapPage';
 
 const App = () => {
   return (
-    <BrowserRouter>
       <Routes>
         <Route path="/" element={<IntroPage />} />
         <Route path="/map/:buildingId" element={<MapPage />} />
 
       </Routes>
-    </BrowserRouter>
   );
 };
 
