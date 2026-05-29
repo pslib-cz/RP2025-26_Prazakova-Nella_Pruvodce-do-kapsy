4537698a2b74a61b0f98db93dceee78ea01cd5f1
Nella Pražáková <nella.prazakova.023@pslib.cz>
2026-05-29 23:38:39 +0200

    Testovani: IntroPage

--- pruvodce.client/src/Pages/IntroPage.tsx
+++ pruvodce.client/src/Pages/IntroPage.tsx
@@ -49,35 +49,70 @@ const IntroPage: React.FC = () => {
       <section className={style.card}>
         <header className={style.header}>
           <a href="https://www.prumyslovkaliberec.cz/" target="_blank" rel="noopener noreferrer">
-            <img src="/logo.png" alt="Průmyslovka Liberec" className={style.logo} />
+            <img src={`${import.meta.env.BASE_URL}logo.png`} alt="Průmyslovka Liberec" className={style.logo} />
           </a>
         </header>
 
         <section className={style.selectorSection}>
-          <div className={style.introText}>
-          <h1 className={style.title}>Vyberte si areál</h1>
-          <p>Zvolte budovu, ve které chcete začít. Areál můžete kdykoli změnit.</p>
 
-</div>
-<div className={style.introContent}>
+          <div className={style.introText}>
+            <div className={style.iconWrapper}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+                <path d="M0 0h24v24H0z" fill="none" />
+                <path fill="none" stroke="#3f4bf7" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m3 7l6-3l6 3l6-3v13l-6 3l-6-3l-6 3zm6-3v13m6-10v13" />
+              </svg>
+
+            <p className={style.subtitle}>
+              Průvodce do kapsy</p>
+              </div>
+            <h1 className={style.title}>Vyberte si <br/><span className={style.blueText}>areál</span></h1>
+            <p>Zvolte budovu, ve které chcete začít. <br/>
+                Areál můžete kdykoli změnit.</p>
+          </div>
+
+          <div className={style.introContent}>
           <div className={style.buttonGroup}>
-            {buildings.map((building, index) => (
+            {buildings.map((building) => (
               <button
                 key={building.buildingId}
                 type="button"
-                className={`${style.buildingButton} ${
-                  index % 2 === 0 ? style.masarykovaButton : style.tyrsovaButton
-                }`}
+                className={style.buildingButton}
                 onClick={() => handleSelectBuilding(building.buildingId)}
               >
-                BUDOVA {building.name.toUpperCase()}
+                <div className={style.buildingButtonContent}>
+                  <h2 className={style.buildingTitle}>
+                    Budova {building.name}
+                  </h2>
+
+                  <div className={style.buildingAddress}>
+                    <svg
+                      className={style.locationIcon}
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path d="M12 21s-6-5.33-6-11a6 6 0 1 1 12 0c0 5.67-6 11-6 11Z" />
+                      <circle cx="12" cy="10" r="2.5" />
+                    </svg>
+
+                    <span>{building.address}</span>
+                  </div>
+                </div>
+
+                <div className={style.buildingArrow}>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 24" className={style.arrowIcon}>
+                    <path d="M0 0h12v24H0z" fill="none" />
+                    <defs>
+                      <path id="SVG1pzpbdYY" fill="#ffffff" d="m7.588 12.43l-1.061 1.06L.748 7.713a.996.996 0 0 1 0-1.413L6.527.52l1.06 1.06l-5.424 5.425z" />
+                    </defs>
+                    <use fillRule="evenodd" href="#SVG1pzpbdYY" transform="rotate(-180 5.02 9.505)" />
+                  </svg>
+                </div>
               </button>
             ))}
           </div>
-
-          <p className={style.quizText}>
-            Nevíte kam vyrazit? <button type="button" className={style.quizLink}>Vyplňte náš kvíz.</button>
-          </p></div>
+        </div>
         </section>
       </section>
     </main>
