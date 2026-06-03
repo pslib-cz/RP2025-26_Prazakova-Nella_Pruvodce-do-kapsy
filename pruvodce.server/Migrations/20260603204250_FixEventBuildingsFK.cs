a85eb5e8b36bbd9bbbcb24bdfc396e6c37b0a21e
Nella Pražáková <nella.prazakova.023@pslib.cz>
2026-06-03 22:44:50 +0200

    Edit migration

---
+++ pruvodce.server/Migrations/20260603204250_FixEventBuildingsFK.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace pruvodce.server.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixEventBuildingsFK : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_EventBuildings_Building_BuildingId",
+                table: "EventBuildings");
+
+            migrationBuilder.DropTable(
+                name: "Building");
+
+            migrationBuilder.DropIndex(
+                name: "IX_EventBuildings_BuildingId",
+                table: "EventBuildings");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
